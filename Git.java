4b4a5e3a1911290a3108f8009c28aa7db551ee21
harsh gupta <harsh.gupta@accolite.com>
2020-06-02 11:37:01 +0530

    git3.java added

--- Git.java
+++ Git.java
@@ -1,5 +1,7 @@
 public class Git{
     
+    public String name;
+
     public static void main(String args[]){
       Git ref = new Git();
     }
--- Git2.java
+++ Git2.java
@@ -1,6 +1,8 @@
 class Git2{
 
     public int id;
+
+    public String name;
     
     public static void main(String args[]){
       Git2 ref = new Git2();
