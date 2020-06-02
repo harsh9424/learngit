e28e86502979041d45cadb7831b396fb20cd97fa
harsh gupta <harsh.gupta@accolite.com>
2020-06-02 11:56:46 +0530

    new commit

--- Git2.java
+++ Git2.java
@@ -6,6 +6,14 @@ class Git2{
 
     public int sum;
     
+    public String lastName;
+
+    public String fullName;
+    
+    public String amount;
+
+    public String rollNo;
+    
     public static void main(String args[]){
       Git2 ref = new Git2();
     }
